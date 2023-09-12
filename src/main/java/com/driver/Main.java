b177d5e9ae04d06c473ef96530735a75526cb871
Shivaksk119 <shivapatelkarru119ksk@gmail.com>
2023-09-12 17:16:10 +0530

    First New Changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
 
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y*1.0;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans1 = p.product(2, 8);
+        int ans2 = p.product(3, 9, 24);
+        double ans3 = p.product(10.3, 8);
+        System.out.println(ans1+" "+ ans2+" "+ ans3);
+
+    }
 }
